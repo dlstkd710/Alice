4e5603c8a649634cf2b65d1f818432b44789bb55
noctics <oncertill@gmail.com>
2016-12-09 09:11:30 +0900

    2016 12  09
    캐릭터 기본 동작 애니메이션 안정화
    
    캐릭터 공격 모션 한번동작하게 만듬

---
+++ 박요한/16.12.07 Portfolio Framework (2)3/Dx3D/cObjMap.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "iMap.h"
+
+class cGroup;
+
+class cObjMap : public iMap
+{
+protected:
+	std::vector<cGroup*>		m_vecGroup;
+	std::vector<D3DXVECTOR3>	m_vecSurface;
+
+public:
+	cObjMap(void);
+	virtual ~cObjMap(void);
+
+	virtual void Load(char* szMap, char* szSurface, D3DXMATRIXA16* pmat = NULL);
+
+	virtual float GetHeight2(IN float x, IN float z) override;
+
+	// iMap
+	virtual void Render() override;
+	virtual bool GetHeight(IN float x, OUT float& y, IN float z) override;
+};
+
