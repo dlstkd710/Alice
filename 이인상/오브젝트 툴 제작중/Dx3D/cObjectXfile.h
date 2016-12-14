e531d06272b22ce5b162ccb6f2b9eef8dd20894b
dlstkd710 <dlstkd710@naver.com>
2016-12-14 13:08:20 +0900

    오브젝트 툴 OBB제작

--- 이인상/오브젝트 툴 제작중/Dx3D/cObjectXfile.h
+++ 이인상/오브젝트 툴 제작중/Dx3D/cObjectXfile.h
@@ -2,17 +2,32 @@
 
 #include "cObjectAllocate.h"
 
+class cOBB;
+
 class cObjectXfile : public cGameObject
 {
 private:
 	ST_BONE*					m_pRootBone;
 	LPD3DXANIMATIONCONTROLLER	m_pAnimController;
+	SYNTHESIZE(D3DXVECTOR3, o_Min, oMin);
+	SYNTHESIZE(D3DXVECTOR3, o_Max, oMax);
+	SYNTHESIZE(bool, o_collision, collision);
 
+	cOBB* o_OBB;
+	cOBB* o_OBB2;
+	cOBB* o_OBB3;
+	cOBB* o_OBB4;
+	cOBB* o_OBB5;
+
+	D3DXMATRIXA16 obb4mat;
 public:
 	cObjectXfile(void);
 	~cObjectXfile(void);
 
-	void Load(std::string sFolder, std::string sFile);
+	void Load(std::string sFolder, std::string sFile, int Num);
+
+	void LoadObb(int PNum);
+
 	void Update();
 	void Render();
 	void SetAnimationIndex(int n);
